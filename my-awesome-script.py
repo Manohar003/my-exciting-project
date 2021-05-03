54a4b2ac95e6090d08a1b11b717d7afada5e4ac4
Manohar003 <manohar4119@gmail.com>
2021-05-03 18:53:59 +0530

    For dropping commit

--- my-awesome-script.py
+++ my-awesome-script.py
@@ -1,7 +1,3 @@
 #!/bin/python
-<<<<<<< HEAD
 #This is an awesome python script by manohar
-=======
-#This is an awesome python script
->>>>>>> 293fe6e67ce3fc6532426fba7cbae9dd8f82e0e6
 print('Howdy, World!')
